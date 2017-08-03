9428d8f1d65626968de73bc2b4701a3224535c54
pupinpants <franklinhlwu@hotmail.com>
2017-08-03 10:54:30 +1000

    wow

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,8 @@
 print("Hello, World!")
 print("This is branch master")
-<<<<<<< HEAD
+
 print("This is branch dev")
-=======
-print("This is a git branch")
->>>>>>> master
+
 print("I hate gedit text editor")
 
 
